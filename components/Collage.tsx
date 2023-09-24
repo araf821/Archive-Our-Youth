1d9b72a25b2f02d7135c20986b6aa1bbaea1ba77
araf821 <araf.ahmed200@gmail.com>
2023-09-24 17:56:54 -0400

    initialized collage item component

--- components/Collage.tsx
+++ components/Collage.tsx
@@ -1,30 +1,15 @@
-"use client";
-
-import { useModal } from "@/hooks/useModal";
 import { dummyItems } from "@/lib/data";
-import Image from "next/image";
 import { FC, Fragment } from "react";
+import CollageItem from "./CollageItem";
 
 interface CollageProps {}
 
 const Collage: FC<CollageProps> = ({}) => {
-  const { onOpen } = useModal();
-
   return (
-    <div className="grid grid-cols-2 items-center gap-2 bg-black sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+    <div className="grid grid-cols-2 items-center overflow-hidden bg-black sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
       {[...dummyItems, ...dummyItems, ...dummyItems].map((item, index) => (
         <Fragment key={index}>
-          <div
-            onClick={() => onOpen("postModal", { post: item })}
-            className={`relative aspect-square cursor-pointer overflow-hidden`}
-          >
-            <Image
-              src={item.imageUrl}
-              alt="collage item"
-              fill
-              className="object-cover transition duration-500 ease-out hover:scale-110"
-            />
-          </div>
+          <CollageItem key={index} item={item} />
         </Fragment>
       ))}
     </div>
--- components/CollageItem.tsx
+++ components/CollageItem.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { useModal } from "@/hooks/useModal";
+import Image from "next/image";
 import { FC } from "react";
 
-interface CollageItemProps {}
+interface CollageItemProps {
+  item: any;
+}
 
-const CollageItem: FC<CollageItemProps> = ({}) => {
-  return <div>CollageItem</div>;
+const CollageItem: FC<CollageItemProps> = ({ item }) => {
+  const { onOpen } = useModal();
+
+  return (
+    <div
+      onClick={() => onOpen("postModal", { post: item })}
+      className={`group relative aspect-square cursor-pointer overflow-hidden opacity-75 transition duration-500 hover:opacity-100`}
+    >
+      <div className="md:duration-500 absolute left-0 top-20 z-10 h-12 w-[350px] translate-x-full rotate-45 bg-white/30 blur-xl transition duration-700 group-hover:-translate-x-full lg:w-[500px]" />
+      <Image
+        src={item.imageUrl}
+        alt="collage item"
+        fill
+        className="object-cover transition duration-500 ease-out hover:scale-110"
+      />
+    </div>
+  );
 };
 
 export default CollageItem;
