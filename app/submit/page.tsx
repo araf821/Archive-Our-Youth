043a33b9fb954059e941c790eb321cde136912fa
araf821 <araf.ahmed200@gmail.com>
2023-09-26 21:27:21 -0400

    set up a few of the steps using react hook form

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -7,38 +7,14 @@ import { ArrowLeft, ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-enum STEPS {
-  WELCOME = 0,
-  TYPE = 1,
-  TITLE = 2,
-  DESCRIPTION = 2,
-  TAGS = 3,
-  CONFIRM = 4,
-}
-
-enum POST_TYPE {
-  TEXT,
-  IMAGE,
-  VIDEO,
-  AUDIO,
-}
-
 const SubmitPage = () => {
   const router = useRouter();
-  const [step, setStep] = useState(STEPS.WELCOME);
-  const [type, setType] = useState<POST_TYPE>();
+  const [step, setStep] = useState(0);
+  // const [type, setType] = useState<POST_TYPE>();
 
-  const onNext = () => {
-    setStep((currentStep) => currentStep + 1);
-  };
-
-  const onBack = () => {
-    setStep((currentStep) => currentStep - 1);
-  };
-
-  const onTypeSelect = (type: POST_TYPE) => {
-    setType(type);
-  };
+  // const onTypeSelect = (type: POST_TYPE) => {
+  //   setType(type);
+  // };
 
   let content = (
     <div className="flex max-w-screen-lg flex-col items-center justify-center gap-12 text-center">
@@ -49,7 +25,6 @@ const SubmitPage = () => {
         Submission Portal
       </p>
       <Button
-        onClick={onNext}
         size="lg"
         className="flex gap-x-2  bg-zinc-200 text-zinc-800 transition hover:translate-x-2 hover:bg-white"
       >
@@ -59,128 +34,128 @@ const SubmitPage = () => {
     </div>
   );
 
-  if (step === STEPS.TYPE) {
-    content = (
-      <div className="space-y-8">
-        <p className="text-2xl md:text-3xl">
-          What type of content would you like to submit?
-        </p>
+  // if (step === 1) {
+  //   content = (
+  //     <div className="space-y-8">
+  //       <p className="text-2xl md:text-3xl">
+  //         What type of content would you like to submit?
+  //       </p>
 
-        {/* types */}
-        <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:gap-8">
-          <button
-            onClick={() => onTypeSelect(POST_TYPE.TEXT)}
-            className={cn(
-              "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
-              {
-                "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
-                  type === POST_TYPE.TEXT,
-              },
-            )}
-          >
-            Text
-          </button>
-          <button
-            onClick={() => onTypeSelect(POST_TYPE.IMAGE)}
-            className={cn(
-              "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
-              {
-                "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
-                  type === POST_TYPE.IMAGE,
-              },
-            )}
-          >
-            Image
-          </button>
-          <button
-            onClick={() => onTypeSelect(POST_TYPE.VIDEO)}
-            className={cn(
-              "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
-              {
-                "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
-                  type === POST_TYPE.VIDEO,
-              },
-            )}
-          >
-            Video
-          </button>
-          <button
-            onClick={() => onTypeSelect(POST_TYPE.AUDIO)}
-            className={cn(
-              "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
-              {
-                "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
-                  type === POST_TYPE.AUDIO,
-              },
-            )}
-          >
-            Audio
-          </button>
-        </div>
+  //       {/* types */}
+  //       <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:gap-8">
+  //         <button
+  //           onClick={() => onTypeSelect(POST_TYPE.TEXT)}
+  //           className={cn(
+  //             "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+  //             {
+  //               "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+  //                 type === POST_TYPE.TEXT,
+  //             },
+  //           )}
+  //         >
+  //           Text
+  //         </button>
+  //         <button
+  //           onClick={() => onTypeSelect(POST_TYPE.IMAGE)}
+  //           className={cn(
+  //             "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+  //             {
+  //               "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+  //                 type === POST_TYPE.IMAGE,
+  //             },
+  //           )}
+  //         >
+  //           Image
+  //         </button>
+  //         <button
+  //           onClick={() => onTypeSelect(POST_TYPE.VIDEO)}
+  //           className={cn(
+  //             "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+  //             {
+  //               "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+  //                 type === POST_TYPE.VIDEO,
+  //             },
+  //           )}
+  //         >
+  //           Video
+  //         </button>
+  //         <button
+  //           onClick={() => onTypeSelect(POST_TYPE.AUDIO)}
+  //           className={cn(
+  //             "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+  //             {
+  //               "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+  //                 type === POST_TYPE.AUDIO,
+  //             },
+  //           )}
+  //         >
+  //           Audio
+  //         </button>
+  //       </div>
 
-        <div className="mx-auto flex w-32 items-center justify-between pt-4">
-          <Button onClick={onBack} variant="link" className="px-0 text-white">
-            <ArrowLeft />
-          </Button>
-          <Button onClick={onNext} variant="link" className="px-0 text-white">
-            <ArrowRight />
-          </Button>
-        </div>
-      </div>
-    );
-  }
+  //       <div className="mx-auto flex w-32 items-center justify-between pt-4">
+  //         <Button onClick={onBack} variant="link" className="px-0 text-white">
+  //           <ArrowLeft />
+  //         </Button>
+  //         <Button onClick={onNext} variant="link" className="px-0 text-white">
+  //           <ArrowRight />
+  //         </Button>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
-  if (step === STEPS.TITLE) {
-    content = (
-      <div className="space-y-8">
-        <p className="text-2xl md:text-3xl">What would you call your work?</p>
+  // if (step === STEPS.TITLE) {
+  //   content = (
+  //     <div className="space-y-8">
+  //       <p className="text-2xl md:text-3xl">What would you call your work?</p>
 
-        <div className="mx-auto w-full max-w-2xl">
-          <input
-            type="text"
-            placeholder="Title of the post"
-            className="w-full border-b border-zinc-600 bg-transparent px-3 py-2 text-xl font-semibold focus:outline-none md:text-2xl"
-          />
-        </div>
+  //       <div className="mx-auto w-full max-w-2xl">
+  //         <input
+  //           type="text"
+  //           placeholder="Title of the post"
+  //           className="w-full border-b border-zinc-600 bg-transparent px-3 py-2 text-xl font-semibold focus:outline-none md:text-2xl"
+  //         />
+  //       </div>
 
-        <div className="mx-auto flex w-32 items-center justify-between pt-4">
-          <Button onClick={onBack} variant="link" className="px-0 text-white">
-            <ArrowLeft />
-          </Button>
-          <Button onClick={onNext} variant="link" className="px-0 text-white">
-            <ArrowRight />
-          </Button>
-        </div>
-      </div>
-    );
-  }
+  //       <div className="mx-auto flex w-32 items-center justify-between pt-4">
+  //         <Button onClick={onBack} variant="link" className="px-0 text-white">
+  //           <ArrowLeft />
+  //         </Button>
+  //         <Button onClick={onNext} variant="link" className="px-0 text-white">
+  //           <ArrowRight />
+  //         </Button>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
-  if (step === STEPS.TAGS) {
-    content = (
-      <div className="space-y-8">
-        <p className="text-2xl md:text-3xl">
-          Choose the tags that best represent your content!
-        </p>
+  // if (step === STEPS.TAGS) {
+  //   content = (
+  //     <div className="space-y-8">
+  //       <p className="text-2xl md:text-3xl">
+  //         Choose the tags that best represent your content!
+  //       </p>
 
-        <div className="mx-auto w-full max-w-2xl">
-          <input
-            type="text"
-            placeholder="Title of the post"
-            className="w-full border-b border-zinc-600 bg-transparent px-3 py-2 text-xl font-semibold focus:outline-none md:text-2xl"
-          />
-        </div>
+  //       <div className="mx-auto w-full max-w-2xl">
+  //         <input
+  //           type="text"
+  //           placeholder="Title of the post"
+  //           className="w-full border-b border-zinc-600 bg-transparent px-3 py-2 text-xl font-semibold focus:outline-none md:text-2xl"
+  //         />
+  //       </div>
 
-        <div className="mx-auto flex w-32 items-center justify-between pt-4">
-          <Button onClick={onBack} variant="link" className="px-0 text-white">
-            <ArrowLeft />
-          </Button>
-          <Button onClick={onNext} variant="link" className="px-0 text-white">
-            <ArrowRight />
-          </Button>
-        </div>
-      </div>
-    );
-  }
+  //       <div className="mx-auto flex w-32 items-center justify-between pt-4">
+  //         <Button onClick={onBack} variant="link" className="px-0 text-white">
+  //           <ArrowLeft />
+  //         </Button>
+  //         <Button onClick={onNext} variant="link" className="px-0 text-white">
+  //           <ArrowRight />
+  //         </Button>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="relative grid h-screen place-items-center bg-zinc-900 text-white">
@@ -190,9 +165,8 @@ const SubmitPage = () => {
       >
         Back to Home
       </Button>
-      <div className="max-w-screen-md px-4 text-center">
-        <PostCreationForm />
-      </div>
+
+      <PostCreationForm />
     </div>
   );
 };
--- components/PostCreationForm.tsx
+++ components/PostCreationForm.tsx
@@ -1,9 +1,284 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/Form";
+import { Button } from "./ui/Button";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ContentType } from "@prisma/client";
+import { cn } from "@/lib/utils";
 
 interface PostCreationFormProps {}
 
+enum STEPS {
+  WELCOME = 0,
+  TYPE = 1,
+  TITLE = 2,
+  CONTENT = 3,
+  TAGS = 4,
+  CONFIRM = 5,
+}
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(1, {
+      message: "Title must be between 1 to 64 characters in length.",
+    })
+    .max(64, {
+      message: "Title must be between 1 to 64 characters in length.",
+    }),
+  contentType: z.enum([
+    ContentType.TEXT,
+    ContentType.IMAGE,
+    ContentType.VIDEO,
+    ContentType.AUDIO,
+  ]),
+  content: z.string().min(3, { message: "Invalid input." }),
+  description: z
+    .string()
+    .min(15, {
+      message: "Description must be between 3 to 2000 characters in length.",
+    })
+    .max(2000, {
+      message: "Description must be between 3 to 2000 characters in length",
+    })
+    .optional(),
+});
+
 const PostCreationForm: FC<PostCreationFormProps> = ({}) => {
-  return <div>PostCreationForm</div>;
+  const router = useRouter();
+  const [step, setStep] = useState(STEPS.WELCOME);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    // @ts-ignore
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      contentType: "TEXT",
+      content: "",
+      description: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  const contentType = form.watch("contentType");
+
+  const onNext = () => {
+    setStep((currentStep) => currentStep + 1);
+  };
+
+  const onBack = () => {
+    setStep((currentStep) => currentStep - 1);
+  };
+
+  let content = (
+    <div
+      className={`flex max-w-screen-lg flex-col items-center justify-center gap-12 text-center ${
+        step > 1 && "hidden"
+      }`}
+    >
+      <p className="flex flex-col gap-2 text-6xl font-bold tracking-wider text-zinc-100 md:text-9xl">
+        Digital<span>Collage</span>
+      </p>
+      <p className="text-xl font-semibold text-zinc-300 md:text-2xl">
+        Submission Portal
+      </p>
+      <Button
+        onClick={onNext}
+        type="button"
+        size="lg"
+        className="fixed bottom-[25vh] flex gap-x-2 bg-zinc-200 text-zinc-800 transition hover:translate-x-2 hover:bg-white"
+      >
+        Get Started
+        <ArrowRight />
+      </Button>
+    </div>
+  );
+
+  if (step === STEPS.TYPE) {
+    content = (
+      <div className="space-y-8">
+        <p className="text-2xl md:text-3xl">
+          What type of content would you like to submit?
+        </p>
+
+        <FormField
+          control={form.control}
+          name="contentType"
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-center justify-center gap-4 md:flex-row md:gap-8">
+              <FormControl>
+                <button
+                  type="button"
+                  onClick={() => form.setValue("contentType", "TEXT")}
+                  className={cn(
+                    "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+                    {
+                      "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+                        contentType === ContentType.TEXT,
+                    },
+                  )}
+                >
+                  Text
+                </button>
+              </FormControl>
+              <FormControl>
+                <button
+                  type="button"
+                  onClick={() => form.setValue("contentType", "IMAGE")}
+                  className={cn(
+                    "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+                    {
+                      "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+                        contentType === ContentType.IMAGE,
+                    },
+                  )}
+                >
+                  Image
+                </button>
+              </FormControl>
+              <FormControl>
+                <button
+                  type="button"
+                  onClick={() => form.setValue("contentType", "VIDEO")}
+                  className={cn(
+                    "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+                    {
+                      "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+                        contentType === ContentType.VIDEO,
+                    },
+                  )}
+                >
+                  Video
+                </button>
+              </FormControl>
+              <FormControl>
+                <button
+                  type="button"
+                  onClick={() => form.setValue("contentType", "AUDIO")}
+                  className={cn(
+                    "w-40 rounded-xl bg-zinc-800 py-2.5 text-zinc-200 transition hover:bg-zinc-700 hover:text-white md:text-lg",
+                    {
+                      "bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950":
+                        contentType === ContentType.AUDIO,
+                    },
+                  )}
+                >
+                  Audio
+                </button>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.TITLE) {
+    content = (
+      <FormField
+        control={form.control}
+        name="title"
+        render={({ field }) => (
+          <FormItem className="space-y-12">
+            <FormLabel className="text-2xl md:text-3xl">
+              What would you name this masterpiece?
+            </FormLabel>
+            <FormControl>
+              <input
+                placeholder="Title of the post"
+                className="w-full border-b border-zinc-600 bg-transparent px-3 py-2 text-center text-2xl font-semibold placeholder:text-center focus:outline-none md:text-3xl"
+                type="text"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="text-red-500" content="asdf" />
+          </FormItem>
+        )}
+      />
+    );
+  }
+
+  if (step === STEPS.CONTENT) {
+    content = (
+      <FormField
+        control={form.control}
+        name="content"
+        render={({ field }) => (
+          <FormItem className="space-y-12">
+            Post Content - {contentType}
+          </FormItem>
+        )}
+      />
+    );
+  }
+
+  if (step === STEPS.TAGS) {
+    content = (
+      <FormField
+        control={form.control}
+        name="content"
+        render={({ field }) => (
+          <FormItem className="space-y-12">Post Tags</FormItem>
+        )}
+      />
+    );
+  }
+
+  if (step === STEPS.CONFIRM) {
+    content = (
+      <FormField
+        control={form.control}
+        name="content"
+        render={({ field }) => (
+          <FormItem className="space-y-12">Confirmation Page</FormItem>
+        )}
+      />
+    );
+  }
+
+  return (
+    <div className="max-w-screen-md px-4 text-center">
+      <Form {...form}>
+        <form onSubmit={() => {}}>{content}</form>
+      </Form>
+      {step > 0 && step < 5 && (
+        <div className="fixed bottom-[15vh] left-0 mx-auto w-full md:bottom-[25vh]">
+          <div className="mx-auto flex w-32 items-center justify-between">
+            <Button
+              type="button"
+              onClick={onBack}
+              variant="link"
+              className="px-0 text-white"
+            >
+              <ArrowLeft />
+            </Button>
+            <Button
+              type="button"
+              onClick={onNext}
+              variant="link"
+              className="px-0 text-white"
+            >
+              <ArrowRight />
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PostCreationForm;
