32e0e3663914014eb6a96171de4dfd13994e2746
araf821 <araf.ahmed200@gmail.com>
2023-10-03 20:33:55 -0400

    implemented dynamic image component

---
+++ components/DynamicImage.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Scaling } from "lucide-react";
+import Image from "next/image";
+import { FC, useState } from "react";
+
+interface DynamicImageProps {
+  src: string;
+}
+
+const DynamicImage: FC<DynamicImageProps> = ({ src }) => {
+  const [contained, setContained] = useState(false);
+
+  return (
+    <div className="relative mt-4 aspect-[4/3] overflow-hidden">
+      <Image
+        fill
+        src={src}
+        alt="post image"
+        className={`rounded-sm ${
+          contained ? "object-contain" : "object-cover"
+        }`}
+      />
+      <button
+        onClick={() => setContained((prev) => !prev)}
+        className="absolute bottom-2 right-2 h-8 w-8 rounded-md bg-zinc-900/40 backdrop-blur-md transition hover:bg-zinc-900 active:scale-90 sm:h-10 sm:w-10"
+      >
+        <Scaling className="h-4 w-4 translate-x-2 md:translate-x-2.5 text-zinc-50 md:h-5 md:w-5" />
+      </button>
+    </div>
+  );
+};
+
+export default DynamicImage;
--- components/modal/PostModal.tsx
+++ components/modal/PostModal.tsx
@@ -2,7 +2,8 @@ import { useRouter } from "next/navigation";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/Dialog";
 import { useModal } from "@/hooks/useModal";
 import Image from "next/image";
-import { X } from "lucide-react";
+import { ExternalLink, X } from "lucide-react";
+import DynamicImage from "../DynamicImage";
 
 const PostModal = ({}) => {
   const { onClose, data, type, isOpen, onOpen } = useModal();
@@ -17,26 +18,23 @@ const PostModal = ({}) => {
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <div className="px-4">
-        <DialogContent className="max-w-screen-md overflow-y-auto bg-transparent px-4">
+        <DialogContent className="max-w-screen-md overflow-y-auto bg-transparent px-4 outline-none">
           <div className="h-full w-full rounded-sm bg-[#202020] p-2 md:rounded-md md:p-6">
-            <div className="flex h-12 w-full items-center justify-between border-b-2 border-zinc-700">
-              <span className="cursor-pointer text-sm text-zinc-400 transition duration-200 hover:text-zinc-200">
-                Expand Post
+            <div className="flex h-8 w-full items-center justify-between border-b-2 border-zinc-700">
+              <span className="flex cursor-pointer items-center gap-2 text-sm text-zinc-400 transition duration-200 hover:text-zinc-200">
+                Expand Post{" "}
+                <ExternalLink className="h-4 w-4 -translate-y-0.5" />
               </span>
-              <X className="w-6 h-6 text-zinc-400 transition duration-200 hover:text-zinc-200"/>
+              <X
+                onClick={onClose}
+                className="h-6 w-6 cursor-pointer text-zinc-400 transition duration-200 hover:text-zinc-200"
+              />
             </div>
             {/* <span className="absolute left-4 top-4 cursor-pointer text-sm text-zinc-400 transition duration-200 hover:text-zinc-200">
             Expand Post
           </span> */}
             {post.contentType === "IMAGE" && (
-              <div className="relative aspect-square overflow-hidden">
-                <Image
-                  fill
-                  src={post.postContent}
-                  alt="post image"
-                  className="rounded-sm object-cover"
-                />
-              </div>
+              <DynamicImage src={post.postContent} />
             )}
             <p className="text-2xl text-zinc-300">{post.title}</p>
           </div>
