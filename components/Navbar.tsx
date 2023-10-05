b547ff59f5810abf9366738e7eb36976350535b6
araf821 <araf.ahmed200@gmail.com>
2023-10-05 00:46:43 -0400

    implemented new navbar + initialized new routes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,12 @@
 
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import Search from "./Search";
 import { User } from "@prisma/client";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useFilters } from "@/hooks/useFilters";
 import { usePathname } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 interface NavbarProps {
   user: User | null;
@@ -51,7 +51,7 @@ const Navbar = ({ user }: NavbarProps) => {
   }, [prevScrollPos]);
 
   return (
-    <div
+    <nav
       className={cn(
         "fixed top-0 z-50 h-[8vh] w-full -translate-y-full border-b border-zinc-800 bg-zinc-900 transition duration-300 hover:opacity-100",
         {
@@ -59,7 +59,7 @@ const Navbar = ({ user }: NavbarProps) => {
         },
       )}
     >
-      <div className="mx-auto flex h-full w-full max-w-screen-2xl items-center justify-between px-4 opacity-100 md:px-12 lg:px-20 xl:px-32">
+      <div className="mx-auto flex h-full w-full max-w-screen-2xl items-center justify-between px-4 opacity-100 md:px-8 xl:px-12">
         <Link
           href="/"
           className="text-xl font-bold text-zinc-100 transition hover:translate-x-1 md:text-2xl"
@@ -67,8 +67,67 @@ const Navbar = ({ user }: NavbarProps) => {
           Digital Collage
         </Link>
 
-        <div className="hidden items-center gap-4 text-zinc-50 md:flex">
-          {/* <Search /> */}
+        <div className="hidden items-center gap-1.5 rounded-full border-2 border-zinc-800 bg-zinc-800 p-2 font-karla tracking-wider text-zinc-50 md:flex">
+          <Link
+            href={"/collage"}
+            className={cn(
+              "rounded-l-full rounded-r-[200rem] px-6 py-1 transition hover:bg-rose-300/10",
+              {
+                "hover:slide-in- bg-gradient-to-tr from-rose-600 to-red-400 text-black shadow-[0_0_20px_2px] shadow-red-500/40 hover:cursor-default":
+                  pathname === "/collage",
+              },
+            )}
+          >
+            Home
+          </Link>
+
+          <div className="h-7 border-l-[1px] border-zinc-700" />
+
+          <Link
+            href={"/about"}
+            className={cn(
+              "rounded-sm px-6 py-1 transition hover:bg-rose-300/10",
+              {
+                "bg-gradient-to-b from-rose-600 to-red-400 text-black shadow-[0_0_20px_2px] shadow-red-500/40 hover:cursor-default hover:bg-rose-500":
+                  pathname === "/about",
+              },
+            )}
+          >
+            About Us
+          </Link>
+
+          <div className="h-7 border-l-[1px] border-zinc-700" />
+
+          <Link
+            href={"/contact"}
+            className={cn(
+              "rounded-sm px-6 py-1 transition hover:bg-rose-300/10",
+              {
+                "bg-gradient-to-b from-rose-600 to-red-400 text-black shadow-[0_0_20px_2px] shadow-red-500/40 hover:cursor-default hover:bg-rose-500":
+                  pathname === "/contact",
+              },
+            )}
+          >
+            Contact
+          </Link>
+
+          <div className="h-7 border-l-[1px] border-zinc-700" />
+
+          <Link
+            href={"/dashboard"}
+            className={cn(
+              "rounded-l-[200rem] rounded-r-full  px-6 py-1 transition hover:bg-rose-300/10",
+              {
+                "bg-gradient-to-tl from-rose-600 to-red-400 text-black shadow-[0_0_20px_2px] shadow-red-500/40 hover:cursor-default hover:bg-rose-500":
+                  pathname === "/dashboard",
+              },
+            )}
+          >
+            Dashboard
+          </Link>
+        </div>
+
+        {/* <div className="hidden items-center gap-4 text-zinc-50 md:flex">
           {pathname === "/collage" && (
             <button
               onClick={() => {
@@ -79,34 +138,21 @@ const Navbar = ({ user }: NavbarProps) => {
               Filters
             </button>
           )}
-        </div>
+        </div> */}
 
         <div className="max-md:hidden md:flex md:items-center md:gap-x-6">
-          {/* <UserButton afterSignOutUrl="/" /> */}
           <Link
             className={cn(
-              "rounded-md border border-zinc-200 px-3.5 py-1.5 text-zinc-200 transition hover:bg-zinc-200 hover:text-zinc-900",
+              "rounded-md bg-zinc-200 px-4 py-1.5 font-bold tracking-widest text-black transition",
             )}
             href={user ? "/submit" : "/sign-in"}
           >
             Submit
           </Link>
-          {user && (
-            <button
-              onClick={() => {}}
-              className="relative aspect-square h-10 w-10 rounded-full bg-zinc-600"
-            >
-              <Image
-                src={user.imageUrl || ""}
-                alt="user profile picture"
-                fill
-                className="rounded-full object-cover"
-              />
-            </button>
-          )}
+          {user && <UserButton afterSignOutUrl="/" />}
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
