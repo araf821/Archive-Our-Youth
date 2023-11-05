a2273cbab31ec3b33af910f2a253e13cd8b229d2
araf821 <araf.ahmed200@gmail.com>
2023-11-05 00:33:03 -0400

    updated post creation route to handle new structure

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -16,11 +16,7 @@ export async function POST(req: Request) {
       content,
       description,
       tags,
-      q1,
-      q2,
-      q3,
-      q4,
-      q5,
+      researchQuestions,
     } = PostCreationValidator.parse(body);
 
     if (!user) {
@@ -37,59 +33,9 @@ export async function POST(req: Request) {
         tags,
         userId: user?.id,
         slug: slugify(title) + nanoid(5),
+        researchQuestions,
       },
     });
-
-    if (q1.trim().length !== 0) {
-      await db.researchQuestion.create({
-        data: {
-          postId: post.id,
-          question: "What is the future you dream of?",
-          answer: q1,
-        },
-      });
-    }
-
-    if (q2.trim().length !== 0) {
-      await db.researchQuestion.create({
-        data: {
-          postId: post.id,
-          question: "What does well-being mean to you right now?",
-          answer: q2,
-        },
-      });
-    }
-
-    if (q3.trim().length !== 0) {
-      await db.researchQuestion.create({
-        data: {
-          postId: post.id,
-          question: "What would you tell your younger or older self?",
-          answer: q3,
-        },
-      });
-    }
-
-    if (q4.trim().length !== 0) {
-      await db.researchQuestion.create({
-        data: {
-          postId: post.id,
-          question: "What's the future you dream of?",
-          answer: q4,
-        },
-      });
-    }
-
-    if (q5.trim().length !== 0) {
-      await db.researchQuestion.create({
-        data: {
-          postId: post.id,
-          question:
-            "What groups, initiatives, projects, and resources support your wellbeing and the wellbeing of the planet? What more would be helpful and of value to you right now?",
-          answer: q5,
-        },
-      });
-    }
 
     return NextResponse.json(post);
   } catch (error) {
