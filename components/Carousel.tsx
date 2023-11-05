fa91dfada8f89d793ab6fec679437917e1931daf
araf821 <araf.ahmed200@gmail.com>
2023-11-04 23:20:50 -0400

    implemented new ui for research questions slide

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -32,19 +32,134 @@ const variants = {
 };
 
 const Carousel = ({}) => {
-  const [[page, direction], setPage] = useState([1, 0]);
+  const [[slide, direction], setSlide] = useState([1, 0]);
   const { userId } = useAuth();
   const { onOpen } = useModal();
 
   const paginate = (newDirection: number) => {
-    setPage([page + newDirection, newDirection]);
+    setSlide([slide + newDirection, newDirection]);
   };
+
+  let slide1, slide2, slide3, slide4;
+
+  slide1 = (
+    <div className="max-w-screen-sm rounded-lg p-8">
+      <p className="z-10 flex select-none flex-col gap-4 py-8 text-center font-karla text-[5rem] font-bold leading-none tracking-tighter text-background text-white opacity-100 md:text-[6rem] lg:text-[7rem]">
+        Digital
+        <span>Archive</span>
+      </p>
+    </div>
+  );
+
+  slide2 = (
+    <div className="mx-4 flex max-w-screen-sm flex-col items-center justify-center gap-3 rounded-lg bg-black/40 px-4 py-4 backdrop-blur-sm">
+      <p className="w-fit font-karla text-4xl font-bold text-white ">
+        Our Story
+      </p>
+      <div className="h-2 w-32 bg-rose-500" />
+      <p className="text-center text-lg font-semibold text-zinc-100 max-md:text-base">
+        Welcome to the{" "}
+        <span className="font-bold text-rose-500">Digital Archive</span> on
+        Youth and Planetary Wellbeing! You&rsquo;re invited to contribute to the
+        Archive and/or explore the multimedia and artifacts preserved here.
+      </p>
+      <p className="text-center text-lg font-semibold text-zinc-100 max-md:text-base">
+        The <span className="font-bold text-rose-500">Digital Archive</span>{" "}
+        explores dream futures, perspectives on wellbeing, and resources both
+        real and desired that support personal to planetary wellbeing. We
+        welcome youth, youth groups and engaged collectives from all over the
+        world to contribute!
+      </p>
+      <button
+        onClick={() => onOpen("submitAuthModal")}
+        className="group my-2 flex w-full max-w-xs justify-between rounded-sm bg-gradient-to-tr from-red-600 to-rose-500 px-4 py-2 text-white shadow-[0_0_10px] shadow-rose-600/50 transition hover:bg-rose-600 hover:shadow-[0_0_25px_2px] hover:shadow-rose-500/50"
+      >
+        Submit A Post
+        <SendHorizonal className="transition group-hover:translate-x-2" />
+      </button>
+    </div>
+  );
+
+  slide3 = (
+    <div className="mx-4 flex max-w-screen-sm flex-col items-center justify-center gap-3 rounded-lg bg-black/40 px-4 py-4 backdrop-blur-sm">
+      <p className="w-fit font-karla text-4xl font-bold text-white ">
+        Our Story
+      </p>
+      <div className="h-2 w-32 bg-rose-500" />
+      <p className="text-center text-lg font-semibold text-zinc-100 max-md:text-base">
+        The <span className="font-bold text-rose-500">Digital Archive</span> was
+        launched as part of the international research study: Partnership for
+        Youth and Planetary Wellbeing, led by the{" "}
+        <a
+          className="font-bold text-blue-400 underline"
+          href="https://younglivesresearch.org"
+          target="_blank"
+        >
+          Young Lives Research Lab
+        </a>
+        , and 4 Youth Advisory Councils (YACs) in Canada, Chile, Costa Rica and
+        Belize.
+      </p>
+      <p className="text-center text-lg font-semibold text-zinc-100 max-md:text-base">
+        The Archive was launched in the Fall of 2023 by Canada&rsquo;s YAC, but
+        the submission portal remains open for continual submissions.
+      </p>
+      <button
+        onClick={() => onOpen("submitAuthModal")}
+        className="group my-2 flex w-full max-w-xs justify-between rounded-sm bg-gradient-to-tr from-red-600 to-rose-500 px-4 py-2 text-white shadow-[0_0_10px] shadow-rose-600/50 transition hover:bg-rose-600 hover:shadow-[0_0_25px_2px] hover:shadow-rose-500/50"
+      >
+        Submit A Post
+        <SendHorizonal className="transition group-hover:translate-x-2" />
+      </button>
+    </div>
+  );
+
+  slide4 = (
+    <div className="pointer-events-auto flex w-full max-w-sm flex-col items-center justify-center gap-2 rounded-md bg-black/40 px-6 py-12 text-center backdrop-blur-sm">
+      <p className="w-fit font-karla text-4xl font-bold text-white ">
+        Let&rsquo;s explore!
+      </p>
+      <p className="text-sm text-zinc-300">
+        Dive right in and showcase what you want or take a look at what others
+        have posted!
+      </p>
+
+      <div className="mt-4 w-full space-y-4">
+        <>
+          {userId ? (
+            <Link
+              href="/submit"
+              className="group flex w-full justify-between rounded-sm bg-gradient-to-tr from-red-600 to-rose-500 px-4 py-2 text-white shadow-[0_0_10px] shadow-rose-600/50 transition hover:bg-rose-600 hover:shadow-[0_0_25px_2px] hover:shadow-rose-500/50"
+            >
+              Submit A Post
+              <SendHorizonal className="transition group-hover:translate-x-2" />
+            </Link>
+          ) : (
+            <button
+              onClick={() => onOpen("submitAuthModal")}
+              className="group flex w-full justify-between rounded-sm bg-gradient-to-tr from-red-600 to-rose-500 px-4 py-2 text-white shadow-[0_0_10px] shadow-rose-600/50 transition hover:bg-rose-600 hover:shadow-[0_0_25px_2px] hover:shadow-rose-500/50"
+            >
+              Submit A Post
+              <SendHorizonal className="transition group-hover:translate-x-2" />
+            </button>
+          )}
+          <Link
+            href="/home"
+            className="group flex w-full justify-between rounded-sm border border-white px-4 py-2 text-white transition hover:bg-zinc-100 hover:text-black"
+          >
+            View All Submissions
+            <Tv className="transition group-hover:translate-x-2" />
+          </Link>
+        </>
+      </div>
+    </div>
+  );
 
   return (
     <div className="flex h-[90vh]">
       <AnimatePresence initial={false} custom={direction}>
         <motion.div
-          key={page}
+          key={slide}
           className="absolute bottom-0 left-0 right-0 top-0 grid place-items-center px-4"
           custom={direction}
           variants={variants}
@@ -52,129 +167,37 @@ const Carousel = ({}) => {
           animate="center"
           exit="exit"
           transition={{
-            x: { type: "spring", stiffness: 300, damping: 30 },
+            x: { type: "spring", stiffness: 300, damping: 25 },
             opacity: { duration: 0.2 },
           }}
         >
-          {page === 1 && (
-            <div className="max-w-screen-sm rounded-lg p-8">
-              <p className="z-10 flex select-none flex-col gap-4 py-8 text-center font-karla text-[5rem] font-bold leading-none tracking-tighter text-background text-white opacity-100 md:text-[6rem] lg:text-[7rem]">
-                Digital
-                <span>Archive</span>
-              </p>
-            </div>
-          )}
-
-          {page === 2 && (
-            <div className="mx-4 flex max-w-screen-sm flex-col items-center justify-center gap-3 rounded-lg bg-black/30 px-4 py-4 backdrop-blur-sm">
-              <p className="w-fit font-karla text-4xl font-bold text-white ">
-                Our Story
-              </p>
-              <div className="h-2 w-32 bg-rose-500" />
-              <p className="text-center text-lg font-semibold text-zinc-100">
-                Welcome to the{" "}
-                <span className="font-bold text-rose-500">Digital Archive</span>{" "}
-                on Youth and Planetary Wellbeing! You&rsquo;re invited to
-                contribute to the Archive and/or explore the multimedia and
-                artifacts preserved here.
-              </p>
-              <p className="text-center text-lg font-semibold text-zinc-100">
-                The{" "}
-                <span className="font-bold text-rose-500">Digital Archive</span>{" "}
-                explores dream futures, perspectives on wellbeing, and resources
-                both real and desired that support personal to planetary
-                wellbeing. We welcome youth, youth groups and engaged
-                collectives from all over the world to contribute!
-              </p>
-            </div>
-          )}
-
-          {page === 3 && (
-            <div className="mx-4 flex max-w-screen-sm flex-col items-center justify-center gap-3 rounded-lg bg-black/30 px-4 py-4 backdrop-blur-sm">
-              <p className="w-fit font-karla text-4xl font-bold text-white ">
-                Our Story
-              </p>
-              <div className="h-2 w-32 bg-rose-500" />
-              <p className="text-center text-lg font-semibold text-zinc-100">
-                The{" "}
-                <span className="font-bold text-rose-500">Digital Archive</span>{" "}
-                was launched as part of the international research study:
-                Partnership for Youth and Planetary Wellbeing, led by the{" "}
-                <a
-                  className="font-bold text-blue-400 underline"
-                  href="https://younglivesresearch.org"
-                  target="_blank"
-                >
-                  Young Lives Research Lab
-                </a>
-                , and 4 Youth Advisory Councils (YACs) in Canada, Chile, Costa
-                Rica and Belize.
-              </p>
-              <p className="text-center text-lg font-semibold text-zinc-100">
-                The Archive was launched in the Fall of 2023 by Canada&rsquo;s
-                YAC, but the submission portal remains open for continual
-                submissions.
-              </p>
-            </div>
-          )}
-
-          {page === 4 && (
-            <div className="pointer-events-auto flex w-full max-w-sm flex-col items-center justify-center gap-2 rounded-sm bg-black/60 px-6 py-12 text-center backdrop-blur-sm">
-              <p className="w-fit font-karla text-4xl font-bold text-white ">
-                Let&rsquo;s explore!
-              </p>
-              <p className="text-sm text-zinc-300">
-                Dive right in and showcase what you want or take a look at what
-                others have posted!
-              </p>
-
-              <div className="mt-4 w-full space-y-4">
-                <>
-                  {userId ? (
-                    <Link
-                      href="/submit"
-                      className="group flex w-full justify-between rounded-sm bg-gradient-to-tr from-red-600 to-rose-500 px-4 py-2 text-white shadow-[0_0_10px] shadow-rose-600/50 transition hover:bg-rose-600 hover:shadow-[0_0_25px_2px] hover:shadow-rose-500/50"
-                    >
-                      Submit A Post
-                      <SendHorizonal className="transition group-hover:translate-x-2" />
-                    </Link>
-                  ) : (
-                    <button
-                      onClick={() => onOpen("submitAuthModal")}
-                      className="group flex w-full justify-between rounded-sm bg-gradient-to-tr from-red-600 to-rose-500 px-4 py-2 text-white shadow-[0_0_10px] shadow-rose-600/50 transition hover:bg-rose-600 hover:shadow-[0_0_25px_2px] hover:shadow-rose-500/50"
-                    >
-                      Submit A Post
-                      <SendHorizonal className="transition group-hover:translate-x-2" />
-                    </button>
-                  )}
-                  <Link
-                    href="/home"
-                    className="group flex w-full justify-between rounded-sm border border-white px-4 py-2 text-white transition hover:bg-zinc-100 hover:text-black"
-                  >
-                    View All Submissions
-                    <Tv className="transition group-hover:translate-x-2" />
-                  </Link>
-                </>
-              </div>
-            </div>
-          )}
+          {slide === 1 ? slide1 : null}
+          {slide === 2 ? slide2 : null}
+          {slide === 3 ? slide3 : null}
+          {slide === 4 ? slide4 : null}
         </motion.div>
       </AnimatePresence>
       <div className="carousel-btn-container z-20 order-last mt-auto space-x-28 md:mb-20 md:space-x-40">
-        {page > 1 && (
+        {slide > 1 && (
           <button
-            className="next"
-            onClick={() => (page === 1 ? null : paginate(-1))}
+            className="next rounded-full bg-black/40 p-1.5 text-zinc-400 transition duration-300 hover:bg-zinc-900 hover:text-white active:scale-90"
+            onClick={() => (slide === 1 ? null : paginate(-1))}
           >
-            <ArrowLeftCircle className="small-on-landscape h-16 w-16 text-white" />
+            <ArrowLeftCircle
+              strokeWidth={1}
+              className="small-on-landscape h-12 w-12 text-white"
+            />
           </button>
         )}
-        {page < 4 && (
+        {slide < 4 && (
           <button
-            className="prev"
-            onClick={() => (page === 4 ? null : paginate(1))}
+            className="prev rounded-full bg-black/40 p-1.5 text-zinc-400 transition duration-300 hover:bg-zinc-900 hover:text-white active:scale-90"
+            onClick={() => (slide === 4 ? null : paginate(1))}
           >
-            <ArrowRightCircle className="small-on-landscape h-16 w-16 text-white" />
+            <ArrowRightCircle
+              strokeWidth={1}
+              className="small-on-landscape h-12 w-12 text-white"
+            />
           </button>
         )}
       </div>
--- components/inputs/ResearchQuestions.tsx
+++ components/inputs/ResearchQuestions.tsx
@@ -34,97 +34,74 @@ const ResearchQuestions: FC<ResearchQuestionsProps> = ({ form }) => {
         What question does your submission answer?{" "}
         <span className="text-zinc-400">(Optional)</span>
       </p>
-      <p className="text-zinc-400 max-md:text-sm">Up to 1000 characters for each question. You do not have to answer them all.</p>
+      <p className="text-zinc-400 max-md:text-sm">
+        This question is for research purposes only.
+      </p>
       <hr className="border-zinc-700" />
-      <div className="">
-        <Accordion className="space-y-2" type="single" collapsible>
-          <AccordionItem className="border-zinc-800" value="1">
-            <AccordionTrigger className="text-base max-md:text-sm">
-              What&rsquo;s the future you dream of?
-            </AccordionTrigger>
-            <FormField
-              name="q1"
-              control={form.control}
-              render={({ field }) => (
-                <AccordionContent>
-                  <FormControl>
-                    <input
-                      placeholder="Your answer... (Optional)"
-                      className="w-full rounded-md bg-zinc-900 px-3 py-2 font-semibold focus:outline-none focus:outline-2 focus:outline-zinc-700 md:text-base"
-                      type="text"
-                      {...field}
-                    />
-                  </FormControl>
-                </AccordionContent>
-              )}
-            />
-          </AccordionItem>
+      <div className="space-y-2">
+        <label
+          htmlFor="q1"
+          className="group relative flex items-center gap-2 rounded-sm bg-zinc-800 px-3 py-1.5 pl-10 md:text-lg"
+        >
+          <input
+            type="checkbox"
+            id="q1"
+            className="absolute left-2 top-2.5 h-5 w-5 appearance-none rounded-md border-2 border-zinc-500 bg-zinc-900 fill-pink-400 checked:border-zinc-900 checked:bg-red-500 group-hover:bg-zinc-700 group-hover:checked:bg-red-700"
+          />
+          What&rsquo;s the future that you dream of?
+        </label>
 
-          <AccordionItem className="border-zinc-800" value="2">
-            <AccordionTrigger className="text-base max-md:text-sm">
-              What does well-being mean to you right now?
-            </AccordionTrigger>
-            <FormField
-              name="q2"
-              control={form.control}
-              render={({ field }) => (
-                <AccordionContent>
-                  <FormControl>
-                    <input
-                      placeholder="Your answer... (Optional)"
-                      className="w-full rounded-md bg-zinc-900 px-3 py-2 font-semibold focus:outline-none focus:outline-2 focus:outline-zinc-700 md:text-base"
-                      type="text"
-                      {...field}
-                    />
-                  </FormControl>
-                </AccordionContent>
-              )}
-            />
-          </AccordionItem>
+        <label
+          htmlFor="q2"
+          className="group relative flex items-center gap-2 rounded-sm bg-zinc-800 px-3 py-1.5 pl-10 md:text-lg"
+        >
+          <input
+            type="checkbox"
+            id="q2"
+            className="absolute left-2 top-2.5 h-5 w-5 appearance-none rounded-md border-2 border-zinc-500 bg-zinc-900 fill-pink-400 checked:border-zinc-900 checked:bg-red-500 group-hover:bg-zinc-700 group-hover:checked:bg-red-700"
+          />
+          What does well-being mean to you right now?
+        </label>
 
-          <AccordionItem className="border-zinc-800" value="3">
-            <AccordionTrigger className="text-base max-md:text-sm">
-              What would you tell your younger or older self?
-            </AccordionTrigger>
-            <FormField
-              name="q3"
-              control={form.control}
-              render={({ field }) => (
-                <AccordionContent>
-                  <FormControl>
-                    <input
-                      placeholder="Your answer... (Optional)"
-                      className="w-full rounded-md bg-zinc-900 px-3 py-2 font-semibold focus:outline-none focus:outline-2 focus:outline-zinc-700 md:text-base"
-                      type="text"
-                      {...field}
-                    />
-                  </FormControl>
-                </AccordionContent>
-              )}
-            />
-          </AccordionItem>
+        <label
+          htmlFor="q3"
+          className="group relative flex items-center gap-2 rounded-sm bg-zinc-800 px-3 py-1.5 pl-10 md:text-lg"
+        >
+          <input
+            type="checkbox"
+            id="q3"
+            className="absolute left-2 top-2.5 h-5 w-5 appearance-none rounded-md border-2 border-zinc-500 bg-zinc-900 fill-pink-400 checked:border-zinc-900 checked:bg-red-500 group-hover:bg-zinc-700 group-hover:checked:bg-red-700"
+          />
+          What would you tell your younger or older self?
+        </label>
 
-          <AccordionItem className="border-zinc-800" value="4">
-            <AccordionTrigger className="text-base max-md:text-sm">
-              What&rsquo;s the future you dream of?
-            </AccordionTrigger>
-            <FormField
-              name="q4"
-              control={form.control}
-              render={({ field }) => (
-                <AccordionContent>
-                  <FormControl>
-                    <input
-                      placeholder="Your answer... (Optional)"
-                      className="w-full rounded-md bg-zinc-900 px-3 py-2 font-semibold focus:outline-none focus:outline-2 focus:outline-zinc-700 md:text-base"
-                      type="text"
-                      {...field}
-                    />
-                  </FormControl>
-                </AccordionContent>
-              )}
-            />
-          </AccordionItem>
+        <label
+          htmlFor="q4"
+          className="group relative flex items-center gap-2 rounded-sm bg-zinc-800 px-3 py-1.5 pl-10 md:text-lg"
+        >
+          <input
+            type="checkbox"
+            id="q4"
+            className="absolute left-2 top-2.5 h-5 w-5 appearance-none rounded-md border-2 border-zinc-500 bg-zinc-900 fill-pink-400 checked:border-zinc-900 checked:bg-red-500 group-hover:bg-zinc-700 group-hover:checked:bg-red-700"
+          />
+          What groups, initiatives, projects, and resources support your
+          wellbeing and the wellbeing of the planet? What more would be helpful
+          and of value to you right now? You can share links to websites and
+          socials, videos, photos, etc.
+        </label>
+
+        <label
+          htmlFor="q5"
+          className="group relative flex items-center gap-2 rounded-sm bg-zinc-800 px-3 py-1.5 pl-10 md:text-lg"
+        >
+          <input
+            type="checkbox"
+            id="q5"
+            className="absolute left-2 top-2.5 h-5 w-5 appearance-none rounded-md border-2 border-zinc-500 bg-zinc-900 fill-pink-400 checked:border-zinc-900 checked:bg-red-500 group-hover:bg-zinc-700 group-hover:checked:bg-red-700"
+          />
+          None of the above
+        </label>
+        {/* <Accordion className="space-y-2" type="single" collapsible>
 
           <AccordionItem className="border-zinc-800" value="5">
             <AccordionTrigger className="text-base max-md:text-sm">
@@ -149,7 +126,7 @@ const ResearchQuestions: FC<ResearchQuestionsProps> = ({ form }) => {
               )}
             />
           </AccordionItem>
-        </Accordion>
+        </Accordion> */}
       </div>
     </div>
   );
