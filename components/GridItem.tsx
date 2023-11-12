090b564ce24562b5a396ed6d8bb49e1e15daaa41
araf821 <araf.ahmed200@gmail.com>
2023-11-12 00:50:59 -0500

    added new colours

--- components/GridItem.tsx
+++ components/GridItem.tsx
@@ -1,115 +1,65 @@
 "use client";
 
-import { FC, useEffect, useState } from "react";
-
-interface GridItemProps {}
+import { useEffect, useState } from "react";
 
 const colors = [
-  "bg-red-500",
-  "bg-blue-500",
-  "bg-amber-500",
-  "bg-green-500",
-  "bg-purple-500",
-  "bg-orange-500",
-  "bg-zinc-900bg-sky-500",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
-  "bg-zinc-900",
+  "bg-[#00294C]",
+  "bg-[#007190]",
+  "bg-[#347F75]",
+  "bg-[#68AEA3]",
+  "bg-[#C1E9E6]",
+  "bg-[#F5DEC7]",
+  "bg-[#FFD9B5]",
+  "bg-[#FFA573]",
+  "bg-[#F86641]",
+  "bg-[#651309]",
+  "bg-[#AB1E10]",
+  "bg-[#D3232A]",
+  "bg-[#DA503E]",
+  "bg-[#FD7361]",
+  "bg-[#E1AE82]",
+  "bg-[#FFC466]",
+  "bg-[#CDD6CB]",
+  "bg-[#FD9747]",
+  "bg-[#1AA5D0]",
+  "bg-[#005371]",
+  "bg-[#69D1E7]",
+  "bg-[#CDEEF4]",
+  "bg-[#6ABB7D]",
+  "bg-[#2B906D]",
+  "bg-[#F8FDE9]",
+  "bg-[#AED0B0]",
+  "bg-[#456034]",
+  "bg-[#456034]",
+  "bg-[#F7B545]",
+  "bg-[#344860]",
 ];
 
-const GridItem: FC<GridItemProps> = ({}) => {
+const GridItem = ({}) => {
+  const [isTransparent, setIsTransparent] = useState(Math.random() < 0.9);
+
   const randomIndex = Math.floor(Math.random() * colors.length);
-  const [classes, setClasses] = useState(colors[randomIndex]);
+
+  const [classes, setClasses] = useState(
+    isTransparent ? "bg-transparent" : colors[randomIndex],
+  );
 
   useEffect(() => {
-    const intervalId = setInterval(changeClasses, 500);
+    const intervalId = setInterval(changeClasses, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
   function changeClasses() {
-    setClasses(colors[Math.floor(Math.random() * colors.length)]);
+    const shouldBecomeTransparent = Math.random() < 0.9;
+    setClasses(
+      shouldBecomeTransparent ? "bg-transparent" : colors[randomIndex],
+    );
+    setIsTransparent(shouldBecomeTransparent);
   }
 
   return (
     <div
-      onClick={() => {
-        alert("clicked");
-        setClasses(colors[Math.floor(Math.random() * colors.length)]);
-      }}
       className={`aspect-square w-full transition duration-1000 ${classes}`}
     />
   );
