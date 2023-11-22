627d4c98ae23571924bde615204ce31d5196f37e
araf821 <araf.ahmed200@gmail.com>
2023-11-22 02:21:31 -0500

    finalized mobile menu

--- components/navbar/MobileMenu.tsx
+++ components/navbar/MobileMenu.tsx
@@ -8,13 +8,14 @@ import { usePathname, useRouter } from "next/navigation";
 import { useAuth, useClerk } from "@clerk/nextjs";
 import { useMenu } from "@/hooks/useMenu";
 import { kobata } from "@/app/fonts";
-import { motion } from "framer-motion";
+import { motion, stagger } from "framer-motion";
+import { useModal } from "@/hooks/useModal";
 interface MobileMenuProps {}
 
 const sidebarVariants = {
   hidden: {
     y: "-100%",
-    opacity: 0.5,
+    opacity: 0,
   },
   show: {
     y: 0,
@@ -23,8 +24,23 @@ const sidebarVariants = {
   },
 };
 
+const buttonVariants = (index: number) => ({
+  hidden: {
+    x: -250,
+  },
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.15 + 0.1 * index,
+      duration: 0.2,
+    },
+  },
+});
+
 const MobileMenu: FC<MobileMenuProps> = ({}) => {
   const { isOpen, onClose } = useMenu();
+  const { onOpen } = useModal();
   const pathname = usePathname();
   const router = useRouter();
   const { userId } = useAuth();
@@ -37,6 +53,9 @@ const MobileMenu: FC<MobileMenuProps> = ({}) => {
       animate={isOpen ? "show" : "hidden"}
       className={cn(
         "fixed inset-0 z-50 flex flex-col bg-zinc-900 px-12 sm:px-20 md:px-28 lg:hidden",
+        {
+          "pointer-events-none": !isOpen,
+        },
       )}
     >
       <div className="pb-4 pt-16">
@@ -47,51 +66,87 @@ const MobileMenu: FC<MobileMenuProps> = ({}) => {
           <X className="sm:h-8 sm:w-8 md:h-10 md:w-10" />
         </button>
       </div>
-      <div className="flex w-full flex-1 flex-col gap-12 pt-20">
-        {navLinks.map((link) => (
-          <Link
-            key={link.label}
-            onClick={onClose}
-            href={link.pathname}
-            className={cn(
-              "w-fit text-3xl font-light text-zinc-100 transition-all hover:tracking-[4px] hover:text-white",
-              {
-                "text-green-500 hover:text-green-500":
-                  pathname === link.pathname,
-              },
-            )}
-          >
-            {link.label}
-          </Link>
-        ))}
-        {userId ? (
-          <button
-            onClick={() =>
-              signOut(() => {
-                onClose();
-                router.push("/home");
-              })
-            }
-            className="w-fit text-3xl font-light text-zinc-100 transition-all hover:tracking-[4px] hover:text-white"
-          >
-            Sign Out
-          </button>
-        ) : (
+      <motion.div
+        animate
+        className="flex w-full flex-1 flex-col gap-8 overflow-hidden pt-12"
+      >
+        <motion.li
+          variants={buttonVariants(1)}
+          animate={isOpen ? "show" : "hidden"}
+          className="list-none"
+        >
           <button
             onClick={() => {
+              if (!userId) {
+                onClose();
+                return onOpen("submitAuthModal");
+              }
               onClose();
-              openSignIn({
-                appearance: {
-                  elements: { modalContent: { paddingTop: 20 } },
-                },
-              });
+              router.push("/submit");
             }}
             className="w-fit text-3xl font-light text-zinc-100 transition-all hover:tracking-[4px] hover:text-white"
           >
-            Sign In
+            Submit A Post
           </button>
-        )}
-      </div>
+        </motion.li>
+        {navLinks.map((link, index) => (
+          <motion.li
+            variants={buttonVariants(index + 2)}
+            animate={isOpen ? "show" : "hidden"}
+            className="list-none"
+            key={link.label}
+          >
+            <button
+              onClick={() => {
+                onClose();
+                router.push(link.pathname);
+              }}
+              className={cn(
+                "w-fit text-3xl font-light text-zinc-100 transition-all hover:tracking-[4px] hover:text-white",
+                {
+                  "text-green-500 hover:text-green-500":
+                    pathname === link.pathname,
+                },
+              )}
+            >
+              {link.label}
+            </button>
+          </motion.li>
+        ))}
+        <motion.li
+          variants={buttonVariants(5)}
+          animate={isOpen ? "show" : "hidden"}
+          className="list-none"
+        >
+          {userId ? (
+            <button
+              onClick={() =>
+                signOut(() => {
+                  onClose();
+                  router.push("/home");
+                })
+              }
+              className="w-fit text-3xl font-light text-zinc-100 transition-all hover:tracking-[4px] hover:text-white"
+            >
+              Sign Out
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                onClose();
+                openSignIn({
+                  appearance: {
+                    elements: { modalContent: { paddingTop: 20 } },
+                  },
+                });
+              }}
+              className="w-fit text-3xl font-light text-zinc-100 transition-all hover:tracking-[4px] hover:text-white"
+            >
+              Sign In
+            </button>
+          )}
+        </motion.li>
+      </motion.div>
       <p className={`${kobata.className} pb-8 text-2xl text-white`}>
         Archive Our Youth
       </p>
