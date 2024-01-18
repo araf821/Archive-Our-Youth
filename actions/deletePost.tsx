01fd7c4c42ab9c3f06d0fd6f83e3b59f57a3de3d
araf821 <araf.ahmed200@gmail.com>
2024-01-17 20:14:42 -0500

    feat: create action for deleting posts

---
+++ actions/deletePost.tsx
@@ -0,0 +1,46 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/getCurrentUser";
+import { revalidatePath } from "next/cache";
+
+export const deletePost = async (
+  postId: string,
+  confirmationString: string,
+) => {
+  const user = await getCurrentUser();
+  if (!user || user.role !== "ADMIN") {
+    return {
+      error: "You don't have access to perform this action.",
+    };
+  }
+
+  const postToDelete = await db.post.findUnique({
+    where: {
+      id: postId,
+    },
+  });
+
+  if (!postToDelete) {
+    return {
+      error: "Post not found.",
+    };
+  }
+
+  if (postToDelete.id.slice(0, 7) !== confirmationString) {
+    return {
+      error: "Confirmation code did not match.",
+    };
+  }
+
+  await db.post.delete({
+    where: {
+      id: postToDelete.id,
+    },
+  });
+
+  revalidatePath("/dashboard/admin-portal/posts");
+  return {
+    success: "Post has been deleted.",
+  };
+};
---
+++ app/(main)/dashboard/admin-portal/_components/post/ConfirmDeletion.tsx
@@ -0,0 +1,95 @@
+import { deletePost } from "@/actions/deletePost";
+import { Button } from "@/components/ui/Button";
+import { Label } from "@/components/ui/Label";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
+
+interface ConfirmDeletionProps {
+  cancel: () => void;
+  postId: string;
+  imageUrl: string;
+}
+
+const ConfirmDeletion = ({
+  cancel,
+  postId,
+  imageUrl,
+}: ConfirmDeletionProps) => {
+  const [confirmationString, setConfirmationString] = useState("");
+  const [isPending, startTransition] = useTransition();
+
+  const onDelete = async () => {
+    startTransition(() => {
+      deletePost(postId, confirmationString)
+        .then((data) => {
+          if (data.success) {
+            toast.success(data.success);
+          }
+
+          if (data.error) {
+            toast.error(data.error);
+          }
+
+          setConfirmationString("");
+          cancel();
+        })
+        .catch(() => {
+          toast.error("Something went wrong.");
+        });
+    });
+  };
+
+  return (
+    <div className="max-md:px-4">
+      <div className="relative mx-auto my-6 aspect-square w-20 md:w-32">
+        <Image
+          src={imageUrl}
+          alt="user profile picture"
+          fill
+          className="rounded-lg object-cover"
+        />
+      </div>
+      <p className="mt-8 text-center text-zinc-300 md:text-lg">
+        Are you sure you want to delete this post?
+      </p>
+      <p className="mb-12 text-center text-zinc-400 max-md:text-sm">
+        You cannot undo this action!
+      </p>
+
+      <div className="w-full space-y-2">
+        <Label className="select-none md:text-base">
+          Type this in to confirm deletion: {postId.slice(0, 7)}
+        </Label>
+        <Input
+          disabled={isPending}
+          className="bg-[#2f2f2f] md:text-base"
+          defaultValue={""}
+          placeholder="1234567"
+          onChange={(e) => setConfirmationString(e.target.value)}
+        />
+      </div>
+      <div className="mt-4 flex w-full gap-4 max-sm:flex-col max-sm:gap-2">
+        <Button
+          disabled={isPending}
+          onClick={cancel}
+          variant="ghost"
+          className="w-full"
+        >
+          Cancel
+        </Button>
+        <Button
+          disabled={isPending || confirmationString !== postId.slice(0, 7)}
+          onClick={onDelete}
+          variant="destructive"
+          className="w-full"
+        >
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmDeletion;
