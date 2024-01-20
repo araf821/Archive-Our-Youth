55c5273d62362a527ed3265ff9bbe06cecd48f47
araf821 <araf.ahmed200@gmail.com>
2024-01-20 16:12:29 -0500

    feat: admin portal single comment component

---
+++ app/(main)/dashboard/admin-portal/_components/comment/Comment.tsx
@@ -0,0 +1,58 @@
+import { Comment, Post, User } from "@prisma/client";
+import CommentDropdown from "./CommentDropdown";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
+import Image from "next/image";
+import { formatDateString } from "@/lib/utils";
+
+interface CommentProps {
+  comment: Comment & { user: User; post: Post };
+}
+
+const Comment = ({ comment }: CommentProps) => {
+  return (
+    <li className="bg-[#2f2f2f] p-4">
+      <div className="flex items-center justify-between gap-8">
+        <div className="flex items-center gap-2">
+          <div className="relative aspect-square w-12 overflow-hidden rounded-md">
+            <Image
+              src={comment.user.imageUrl || "/placeholder-image.png"}
+              alt="user image"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="">
+            <p>{comment.user.name}</p>
+            <p className="text-xs text-zinc-400 md:text-sm">
+              Commented on{" "}
+              {formatDateString(comment.createdAt.toISOString(), {
+                hideTime: true,
+              })}
+            </p>
+          </div>
+        </div>
+
+        <CommentDropdown />
+      </div>
+
+      <p className="mt-2.5 text-zinc-400">Likes: {comment.likes}</p>
+      <p className="flex items-center gap-1 text-sm text-zinc-400 md:text-base">
+        Posted under:
+        <Link
+          className="flex items-center gap-1 font-medium tracking-wider text-zinc-200"
+          href={""}
+        >
+          {comment.post.title}
+          <ExternalLink className="h-4 w-4" />
+        </Link>
+      </p>
+
+      <div className="mt-4 whitespace-pre-line rounded-sm bg-[#212121] p-2 max-md:text-sm md:p-3">
+        {comment.content}
+      </div>
+    </li>
+  );
+};
+
+export default Comment;
--- app/(main)/dashboard/admin-portal/_components/comment/CommentList.tsx
+++ app/(main)/dashboard/admin-portal/_components/comment/CommentList.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import CommentDropdown from "./CommentDropdown";
 import Link from "next/link";
 import { ExternalLink } from "lucide-react";
+import Comment from "./Comment";
 
 interface CommentListProps {}
 
@@ -17,50 +18,16 @@ const CommentList = async ({}: CommentListProps) => {
       user: true,
       post: true,
     },
+    orderBy: {
+      createdAt: "desc",
+    },
   });
 
   return (
-    <ul className="my-2.5 flex flex-col gap-2.5">
-      <li className="bg-[#2f2f2f] p-4">
-        <div className="flex items-center justify-between gap-8">
-          <div className="flex items-center gap-2">
-            <div className="relative aspect-square w-12 overflow-hidden rounded-md">
-              <Image
-                src={comments[0].user.imageUrl || "/placeholder-image.png"}
-                alt="user image"
-                fill
-                className="object-cover"
-              />
-            </div>
-            <div className="">
-              <p>{comments[0].user.name}</p>
-              <p className="text-xs text-zinc-400 md:text-sm">
-                Commented on{" "}
-                {formatDateString(comments[0].createdAt.toISOString(), {
-                  hideTime: true,
-                })}
-              </p>
-            </div>
-          </div>
-
-          <CommentDropdown />
-        </div>
-
-        <p className="mt-2.5 flex items-center gap-1 text-sm text-zinc-400 md:text-base">
-          Post:
-          <Link
-            className="flex items-center gap-1 font-medium tracking-wider text-zinc-200"
-            href={""}
-          >
-            {comments[0].post.title}
-            <ExternalLink className="h-4 w-4" />
-          </Link>
-        </p>
-
-        <div className="mt-4 whitespace-pre-line bg-[#212121] p-2 max-md:text-sm md:p-3">
-          {comments[0].content}
-        </div>
-      </li>
+    <ul className="my-4 flex flex-col gap-2.5">
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
     </ul>
   );
 };
