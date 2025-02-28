8c094031b8a29f48147424b94ad3bbc94f79ca7f
araf821 <araf.ahmed200@gmail.com>
2025-02-27 20:07:47 -0500

    refactor: move file uploader logic to a separate configuration file

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,47 +1,5 @@
-import { getCurrentUser } from "@/lib/getCurrentUser";
-import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { utapi } from "@/lib/uploadthing-utils";
-
-const f = createUploadthing();
-
-export const fileUploader = {
-  image: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async () => {
-      const currentUser = await getCurrentUser();
-      return { userId: currentUser?.userId };
-    })
-    .onUploadComplete(async () => {}),
-
-  video: f({ video: { maxFileSize: "32MB" } })
-    .middleware(async () => {
-      const currentUser = await getCurrentUser();
-      return { userId: currentUser?.userId };
-    })
-    .onUploadComplete(async () => {}),
-
-  audio: f({ audio: { maxFileSize: "8MB" } })
-    .middleware(async () => {
-      const currentUser = await getCurrentUser();
-      return { userId: currentUser?.userId };
-    })
-    .onUploadComplete(async () => {}),
-
-  thumbnail: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async () => {
-      const currentUser = await getCurrentUser();
-      return { userId: currentUser?.userId };
-    })
-    .onUploadComplete(async () => {}),
-
-  pdf: f({ pdf: { maxFileSize: "8MB" } })
-    .middleware(async () => {
-      const currentUser = await getCurrentUser();
-      return { userId: currentUser?.userId };
-    })
-    .onUploadComplete(async () => {}),
-} satisfies FileRouter;
-
-export type OurFileRouter = typeof fileUploader;
+import { fileUploader } from "@/lib/config/uploadthing";
 
 export async function DELETE(request: Request) {
   try {
---
+++ lib/config/uploadthing.ts
@@ -0,0 +1,43 @@
+import { getCurrentUser } from "@/lib/getCurrentUser";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const fileUploader = {
+  image: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async () => {
+      const currentUser = await getCurrentUser();
+      return { userId: currentUser?.userId };
+    })
+    .onUploadComplete(async () => {}),
+
+  video: f({ video: { maxFileSize: "32MB" } })
+    .middleware(async () => {
+      const currentUser = await getCurrentUser();
+      return { userId: currentUser?.userId };
+    })
+    .onUploadComplete(async () => {}),
+
+  audio: f({ audio: { maxFileSize: "8MB" } })
+    .middleware(async () => {
+      const currentUser = await getCurrentUser();
+      return { userId: currentUser?.userId };
+    })
+    .onUploadComplete(async () => {}),
+
+  thumbnail: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async () => {
+      const currentUser = await getCurrentUser();
+      return { userId: currentUser?.userId };
+    })
+    .onUploadComplete(async () => {}),
+
+  pdf: f({ pdf: { maxFileSize: "8MB" } })
+    .middleware(async () => {
+      const currentUser = await getCurrentUser();
+      return { userId: currentUser?.userId };
+    })
+    .onUploadComplete(async () => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof fileUploader;
