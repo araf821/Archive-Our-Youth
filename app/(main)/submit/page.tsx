4566a26a0f302b385a6726ef10922264388656ee
araf821 <araf.ahmed200@gmail.com>
2025-03-08 19:11:00 -0500

    fix: add flex-col to submit page content

--- app/(main)/submit/page.tsx
+++ app/(main)/submit/page.tsx
@@ -14,7 +14,7 @@ const SubmitPage = async () => {
   await initializeUser();
 
   return (
-    <div className="mx-auto my-12 flex max-w-3xl space-y-8 px-4 md:my-20">
+    <div className="mx-auto my-12 flex max-w-3xl flex-col space-y-8 px-4 md:my-20">
       <IntroScreen />
       <PostCreationForm />
     </div>
