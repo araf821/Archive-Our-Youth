ab82f8e07e242cb8cc29adeac9e2f8322d849546
araf821 <araf.ahmed200@gmail.com>
2025-03-20 02:34:42 -0400

    fix: remove unnecessary secret check from uploadthing-utils

--- lib/uploadthing-utils.ts
+++ lib/uploadthing-utils.ts
@@ -1,10 +1,5 @@
 import { UTApi } from "uploadthing/server";
 
-if (!process.env.UPLOADTHING_SECRET) {
-  throw new Error("UPLOADTHING_SECRET is required");
-}
-
-// Initialize the UploadThing API client with the secret token
 export const utapi = new UTApi({
   token: process.env.UPLOADTHING_TOKEN,
 });
